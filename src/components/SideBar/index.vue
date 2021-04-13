acc78450e620232294ca6aa2fea7015890282b96
freaklucas <lucas.oliveira.freak@gmail.com>
2021-04-13 11:11:34 -0300

    alinhando sidebar para refatorar

--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -4,35 +4,35 @@
             <icon name="twitter-brand" />
         </button>
         <div class="w-1/5 px-8 py-2">
-            <button :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3`">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-home" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Home</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-search" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Explorar</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-notification" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Notificações</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-message" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-2">Mensagens</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 mr-1">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-save" />
-                <p class="text-lg font-semibold text-left hidden lg:block ml-2" >Itens Salvos</p>
+                <p class="text-lg font-semibold whitespace-nowrap text-left hidden lg:block ml-1" >Itens Salvos</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-list" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Listas</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-profile" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Perfil</p>
             </button>
-            <button class="focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3">
+            <button class="flex space-x-2 items-center text-white-600 p-2 hover:bg-blue-200 rounded-lg hover:text-blue-900 focus:outline-none mb-2">
                 <icon name="twitter-more" />
                 <p class="text-lg font-semibold text-left hidden lg:block ml-1">Mais</p>
             </button>
