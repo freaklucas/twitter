c29a53e6db1524b1d742583e8f1a40b06ca6b3f5
freaklucas <lucas.oliveira.freak@gmail.com>
2021-04-16 14:56:32 -0300

    escondendo dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="flex container h-screen w-full">
-    <div class=" w-1/5 border-r border-lighter px-8 py-2 flex flex-col justify-between">
+    <div class="lg:w-1/5 border-r border-lighter lpx-2 lg:px-8 py-2 flex flex-col justify-between">
       <div>
         <button class="focus:outline-none h-12 w-12 hover:bg-lightblue text-3xl  rounded-full text-blue">
           <i class="fab fa-twitter"></i>
@@ -13,18 +13,20 @@
             <i :class="`${tab.icon}  text-2xl mr-4 text-left `"></i>
             <p class="text-lg font-semibold text-left">{{ tab.title }}</p>
           </button>
-          <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-full p-3 hover:bg-darkblue">
-            Tweet
+          <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-12 h-12 lg:h-auto lg:w-full p-3 hover:bg-darkblue">
+            <p class="hidden lg:block">Tweetar</p>
+            <i class="fas fa-plus lg:hidden"></i>
           </button>
         </div>
       </div>
-      <div class="w-full relative">
+      <div class="lg:w-full relative">
         <button @click="dropdown = true"  class="flex items-center w-full hover:bg-lightblue rounded-full p-2 focus:outline-none">
           <img src="../src/assets/images/profile.png" class="w-12 h-12 rounded-full border-lighter"/>
-          <div class="ml-4">
+          <div class="hidden lg:block ml-4">
             <p class="text-sm font-bold leading-tight">William Bonner</p>
             <p class="text-sm leading-tight">@williambonner</p>
           </div>
+          <i class="hidden lg:block fas fa-angle-down ml-auto text-lg"></i>
         </button>
         <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
           <button @click="dropdown = false"  class="p-3 flex items-center w-full hover:bg-lightest  p-2 focus:outline-none">
@@ -36,10 +38,10 @@
           <i class="fas fa-check ml-auto test-blue"></i>
         </button>
         <button @click="dropdown = false" class="w-full text-left hover:bg-lightest border-t border-lighter p-3 test-sm focus:outline-none">
-          Adicione uma conta existente
+          Adicionar uma conta existente
         </button>
         <button @click="dropdown = false" class="w-full text-left hover:bg-lightest border-t border-lighter p-3 test-sm focus:outline-none">
-          Entre como @williambonner
+          Sair de @williambonner
         </button>
         </div>
       </div>
