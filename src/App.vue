ab39412295dfd470764922e670393f97bd090288
freaklucas <lucas.oliveira.freak@gmail.com>
2021-04-19 00:57:46 -0300

    criando array de publicação

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
         </div>
         <form class="w-full px-4 relative">
           <textarea placeholder="O que está acontecendo?" 
-            class="w-full focus:outline-none"
+            class="mt-3 pb-3 w-full focus:outline-none"
           />
           <div class="flex items-center">
             <i class="text-lg text-blue mx-6 far fa-image"></i>
@@ -22,7 +22,18 @@
             <i class="text-lg text-blue mx-6 far fa-chart-bar"></i>
             <i class="text-lg text-blue mx-6 far fa-smile"></i>
           </div>
+          <button 
+            class="h-10 px-4 text-white font-semibold bg-blue hover:bg-darkblue 
+              focus:outline-none rounded-full absolute bottom-0 right-0"
+          >
+            Tweetar
+          </button>
         </form>
+      </div>
+      <div class="w-full p-4 border-b hover:bg-lighter flex">
+        <div class="flex-none mr-4">
+          <img src="" class="h-12 w-12 rounded-full flex-none"/>
+        </div>
       </div>
     </div>
     <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
@@ -41,7 +52,13 @@ export default {
   name: 'App',
   components: { SideBar, TrendingTopic, WhoFollow },
   data() {
-    return {}
+    return {
+      following: [
+        { src: 'fausto.png', name: 'Fausto Silva', handle: '@faustosilva', time: '20s', tweet: 'Oloco meu!!!', coments: '1.000', retweets: '400', like: '9.000' },
+        { src: 'supla.png', name: 'Supla', handle: '@Sulpla', time: '70s', tweet: 'Yeah, its is amazing cara!', coments: '700', retweets: '1.400', like: '1.000' },
+        { src: 'tiririca.png', name: 'Tiririca sem sobrenome', handle: '@tiririca', time: '20 min', tweet: 'Queria fazer piada, mas piada não posso fazer, pois quem faz piada não faz lei e lei eu preciso de vc!', coments: '500', retweets: '100', like: '3.000' }
+      ]
+    }
   }
 }
 </script>
