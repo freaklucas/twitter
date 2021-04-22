5ac76b150a9247181d6251ca6e3cd9c8a9ef10d2
freaklucas <lucas.oliveira.freak@gmail.com>
2021-04-22 19:34:23 -0300

    refatorando a timeline de home

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app" class="flex container h-screen w-full">
-    <side-bar/>
-    <div class="w-1/2 h-full overflow-y-scrool">
-      <timeline />
+    <div id="app">
+      <home />
     </div>
-    <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
-      <trending-topic />  
-      <who-follow />
-    </div>
-  </div>
 </template>
 
 <script>
-import SideBar from './components/SideBar'
-import TrendingTopic from './components/TrendingTopic'
-import WhoFollow from './components/WhoFollow'
-import Timeline from './components/Timeline'
+import Home from './views/Home'
 
 export default {
+  components: {Home},
   name: 'App',
-  components: { SideBar, TrendingTopic, WhoFollow, Timeline },
-  data() {
-    return {}
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+import router from './router'
+
 import './index.css'
 import './assets/styles/index.css'
 import '@/assets/css/tailwind.css'
 
-
-Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
