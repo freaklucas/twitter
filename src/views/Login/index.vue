c1716c180866baf20213e5d4d89ba5528e9ecf57
freaklucas <lucas.oliveira.freak@gmail.com>
2021-04-24 20:09:32 -0300

    refatorando tamanhos do login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,24 +1,24 @@
 <template>
-    <div>
+    <div class="overflow-hidden">
         <div class="flex flex-row">
-            <div class="flex w-3/5 w-full flex-row">
+            <div class="flex lg:w-3/5 w-full flex-row md:w-1/2">
                 <div class="flex flex-col md:flex-row h-screen">
                     <img src="../../../src/assets/images/login.png" alt="imagem do twitter" class="">
                 </div>
             </div>
-            <div class="flex flex-col justify-center w-3/5 w-full py-10">
-                <div class="mb-5 mt-10 ml-16 px-6">
+            <div class="flex flex-col justify-center lg:w-3/5 w-full md:w-1/2">
+                <div class="mb-2 mt-6 ml-16 px-6">
                     <button class="focus:outline-none h-30 w-30  text-5xl  rounded-full text-blue">
                         <i class="fab fa-twitter h-30"></i>
                     </button>
                 </div>
-                <h1 class="font-bold text-7xl mb-1 mt-1 ml-11 p-6">Acontecendo agora</h1>
-                <h2 class="font-bold text-2xl ml-11 p-16">Inscreva-se no twitter hoje mesmo.</h2>
-                <div class="container ml-12 px-9 h-12">
-                    <button class="px-8 py-3 text-1xl font-bold rounded-full bg-blue focus:outline-none ">Inscrever-se</button>
+                <h1 class="font-extrabold text-7xl mb-1 mt-1 ml-11 p-6 md:text-5xl">Acontecendo agora</h1>
+                <h2 class="font-bold text-3xl ml-11 p-6">Inscreva-se no twitter hoje mesmo.</h2>
+                <div class="container ml-12 px-9 h-12 mb-7">
+                    <button class="px-8 py-5 text-2xl text-center h-15 w-4/6 font-bold rounded-full bg-blue hover:shadow focus:outline-none ">Inscrever-se</button>
                 </div>
                 <div class="container py-2 ml-11  px-9">
-                    <button class="px-8 py-3 text-1xl h-12 font-bold rounded-full bg-blue focus:outline-none">Entrar</button>
+                    <button class="px-8 py-5 text-2xl text-center h-15 w-4/6 font-bold rounded-full bg-lightblue hover:shadow-lg focus:outline-none">Entrar</button>
                 </div>
             </div>
         </div>
@@ -38,4 +38,9 @@
 export default {
     
 }
+
 </script>
+
+<style lang="postcss" scoped>
+
+</style>
