845c381baf56892da8ff3d5e2a136c4efb308a13
freaklucas <lucas.oliveira.freak@gmail.com>
2021-05-04 00:58:31 -0300

    configurando mobile

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,7 +2,7 @@
     <div class="overflow-hidden">
         <div class="flex flex-row">
             <div class="flex lg:w-3/5 w-full flex-row md:w-1/2 hidden md:flex hidden md:block">
-                <div class="flex flex-col md:flex-row h-screen">
+                <div class="flex flex-col md:flex-row h-screen container">
                     <img src="../../../src/assets/images/twitter.png" alt="imagem do twitter" class="w-full sm:block flex">
                 </div>
             </div>
@@ -15,10 +15,10 @@
                 <h1 class="font-extrabold lg:text-7xl align-text-middle mb-1 mt-1 pl-6 pb-14 md:text-5xl text-4xl">Acontecendo agora</h1>
                 <h2 class="font-extrabold lg:text-4xl align-text-middle pl-7 pb-7 md:text-3xl text-3xl">Inscreva-se no twitter hoje mesmo.</h2>
                 <div class="container ml-12 px-9 h-12 mb-9">
-                    <button class="px-8 py-7 lg:text-2xl md:text-1xl text-center h-15 w-4/6 font-bold rounded-full bg-blue hover:shadow focus:outline-none text-1xl ">Inscrever-se</button>
+                    <button class="px-8 py-6 lg:text-2xl md:text-1xl text-center h-15 w-4/6 font-bold rounded-full bg-blue hover:shadow focus:outline-none sm:whitespace-nowrap">Inscrever-se</button>
                 </div>
                 <div class="container py-5 ml-11  px-9">
-                    <button class="px-8 py-5 lg:text-2xl md:text-1xl text-center h-15 w-4/6 font-bold rounded-full bg-lighter hover:bg-blue focus:outline-none text-1xl">Entrar</button>
+                    <button class="px-8 py-7 lg:text-2xl md:text-1xl text-center h-15 w-4/6 font-bold rounded-full bg-lighter hover:bg-blue focus:outline-none">Entrar</button>
                 </div>
             </div>
         </div>
